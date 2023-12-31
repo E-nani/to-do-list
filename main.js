ea6dd6ca4a721862b0bddc24416ddd4a37b631fb
In Hwan Hwang <ytew04357@gmail.com>
2024-01-01 07:18:15 +0900

    Adding Choice Not done tab

--- main.js
+++ main.js
@@ -12,6 +12,28 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList=[];
+let tabs = document.querySelectorAll(".task-tabs div");
+
+// ("")안에 조건에 맞는 모든 것을 가져옴 -> id name등에 제한 받지 않음
+// querySelector랑 차이는 한개냐 여러개냐 
+// (#sections) -> sections 아이디를 가진 요소를 찾음.
+// (.section) -> section 클래스명을 가진 요소를 찾음.
+// ex) querySelector
+// let section = document.querySelector("#sections .section");
+// ex) querySelectorAll
+// let sections = document.querySelectorAll("#sections, #sections .section");
+// console.log(tabs) div갯수확인
+
+let mode = 'all' // -> Html에서 div All탭 id 값
+let filterList = [];
+
+for(let i=1; i<tabs.length; i++){ //i=1부터인 이유는 0번쨰 div는 underline이라 필요없음
+    tabs[i].addEventListener("click", function(event){
+        filter(event)
+    })
+    // add이벤트는 클릭하면 function event안에 filter 적용
+    // 내가 무슨 tab을 선택했는지 알아야해서 function event사용
+}
 
 
 addButton.addEventListener("click",addTask);
@@ -32,7 +54,7 @@ function addTask(){
     }
     taskList.push(task);
     //taskList.push(taskContent); task 객체때문에 필요없어져서 ()안에 task로 대체
-    console.log(taskList)
+    //console.log(taskList)
     render();
     
     //console.log("clicked"); //+버튼 눌럿을때 쿨릭되는지 확인
@@ -40,25 +62,59 @@ function addTask(){
 }
 
 function render(){ // 그림 이벤트는 render로 //값이 업데이트되면 UI(render)도: 함수에 render넣으면됨
+    //내가 선택한 탭에 따라서 리스트를 달리 보여준다
+    // 지금은 taskList만 가능 
+    // all만 보여줄꺼면 tasklist만 보여줘도 되지만
+    // not done done을 보여줄려면 filterList도 필요
+    // 내가 선택한 것에 대한 정보는 mode가 가지고 있음 
+    let list =[]; // 리스트를 달리 보여주기 위해
+    // 내가 선택한 탭 만드는 식 (아래)
+    if(mode ==="all"){ // all -> tasklist
+        list = taskList;
+    } else if(mode ==="not done"){ //not done done -> filterList
+        list = filterList;
+    }
+    
     let resultHTML = '';
-    for(let i =0; i<taskList.length; i++){
-        if(taskList[i].isComplete == true){//tasklist안에 iscomplete가 true라면
+    for(let i =0; i<list.length; i++){
+        if(list[i].isComplete == true){//tasklist안에 iscomplete가 true라면
             resultHTML +=  `<div class="task">
-        <div class="task-done">${taskList[i].taskContent}</div>
+        <div class="task-done">${list[i].taskContent}</div>
         <div>
-            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-            <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            <button onclick="toggleComplete('${list[i].id}')">Check</button>
+            <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
     </div>`
         }else{
         resultHTML +=  `<div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-            <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            <button onclick="toggleComplete('${list[i].id}')">Check</button>
+            <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
     </div>`
         }
+
+
+    // let resultHTML = '';
+    // for(let i =0; i<taskList.length; i++){
+    //     if(taskList[i].isComplete == true){//tasklist안에 iscomplete가 true라면
+    //         resultHTML +=  `<div class="task">
+    //     <div class="task-done">${taskList[i].taskContent}</div>
+    //     <div>
+    //         <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+    //         <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+    //     </div>
+    // </div>`
+    //     }else{
+    //     resultHTML +=  `<div class="task">
+    //     <div>${taskList[i].taskContent}</div>
+    //     <div>
+    //         <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+    //         <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+    //     </div>
+    // </div>`
+    //     }
     // taskList[i]만 넣으면 task 객체때문에(string이아니라서) object로 출력됨
     // 따라서 taskList[i].taskContent로 정확하게 객체안에 있는
     // taskContent만 찍어줘서 정상적으로 출력되게 만들어줌
@@ -103,8 +159,47 @@ function deleteTask(id){
         taskList.splice(i,1); //i번째 1개 삭제
         break;
     }
-    render();
    }
+   render(); //위치 중요
+}
+
+function filter(event){ 
+    // 매개변수를 addeventListener로부터 event를 받음 
+    // event안에는 내가 누구를 클릭했는지에 대한 정보가 있음
+    // all not done done div각각을 구분하기 위해 id를 줘야함
+    // 고정된 값(=탭)이라 html에 바로줌
+    console.log("filter",event.target.id);
+    // .target정확하게 뭘클릭했는지 보여줌 ex) <div id = not done> Not Done </div>
+    //.target.id 아이디 값만 가져옴 ex) not done
+    
+    mode = event.target.id; 
+    // 너무 길어서 줄여줌
+    //필요한 이유는 바로위 기술
+    // mode값은 render에서도 필요해서 지역변수가 아닌 전역변수로 바꿔줌
+    filterList = []; 
+    // 필터된 리스트 모음이 필요함 render도 필요해서 전역변수로
+    // 다시 비워주지 않으면 클릭할때마다 반복되서 ui에 계속 not done이 반복됨
+    // ex) not done누르면 1,3나오면 다시누르면 1,3,1,3나옴
+    if(mode == "all"){ //event.target.id all이면
+        //전체리스트 보여줌
+        render(); // render가 사실상 전체리스트 보여줌
+    } else if(mode == "not done"){
+        //Not done인 아이템을 보여줌
+        // 진행중은 task.isComplete=false  
+        // 진행중인건 컴퓨터가 구분못하기 때문
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == false){
+                // taskList안에 i번째 값이 false다 == 진행중의미
+                filterList.push(taskList[i])
+                // 필터된 리스트 모음이 필요해서 i번째값을 저장
+            }
+        }
+        render(); //UI바꿔주는 기능
+        console.log("진행중",filterList);
+    } else if(mode === "done"){
+        // done인 아이템을 보여줌
+        // 끝난것은 task.isComplete=true 
+    }
 }
 
 function randomIDGenerate(){
